86164523b9927e948eeb795f969078c98348072e
shanthishan <shanthi.viji66@gmail.com>
2023-04-05 23:12:55 +0530

    Test

---
+++ Addition.java
@@ -0,0 +1,8 @@
+public class Addition {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int a = 10;
+        System.out.println(a);
+        //56
+    }
+}
